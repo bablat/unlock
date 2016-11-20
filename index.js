446e65ea2b2723eef4eb6c7cb74465d2cfa13ce4
bablat <ben426@gmail.com>
2016-11-20 19:22:01 +0000

    more

--- index.js
+++ index.js
@@ -1,10 +1,57 @@
-var express = require('express')
-var app = express()
+const path = require('path')
+const exphbs = require('express-handlebars')
 
-app.get('/', function (req, res) {
-  res.send('Hello World!')
-})
 
-app.listen(3000, function () {
-  console.log('Example app listening on port 3000!')
+var express = require('express'),
+    bodyParser = require('body-parser'),
+    form = require('express-form'),
+    field = form.field;
+ 
+var app = express();
+app.use(bodyParser());
+
+app.engine('.hbs', exphbs({  
+  defaultLayout: 'main',
+  extname: '.hbs',
+  layoutsDir: path.join(__dirname, 'views/layouts')
+}))
+app.set('view engine', '.hbs')  
+app.set('views', path.join(__dirname, 'views'))  
+
+app.get('/', (request, response) => {  
+  response.render('home', {
+    name: 'John'
+  })
 })
+ 
+/*
+app.post(
+ 
+  // Route 
+  '/user',
+ 
+  // Form filter and validation middleware 
+  form(
+    field("username").trim().required().is(/^[a-z]+$/),
+    field("password").trim().required().is(/^[0-9]+$/),
+    field("email").trim().isEmail()
+   ),
+ 
+   // Express request-handler now receives filtered and validated data 
+   function(req, res){
+     if (!req.form.isValid) {
+       // Handle errors 
+       console.log(req.form.errors);
+ 
+     } else {
+       // Or, use filtered form data from the form object: 
+       console.log("Username:", req.form.username);
+       console.log("Password:", req.form.password);
+       console.log("Email:", req.form.email);
+     }
+  }
+);
+*/
+
+app.listen(3000);
+
