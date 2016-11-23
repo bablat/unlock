970de7831713ae47107b3858ca21df485e7927d6
bablat <ben426@gmail.com>
2016-11-23 11:58:10 +0000

    removed semicolons

--- unlock.js
+++ unlock.js
@@ -1,21 +1,19 @@
 const path = require('path')
 const exphbs = require('express-handlebars')
 
-var fs = require('fs');
-var push = require('pushover-notifications');
+var fs = require('fs')
+var push = require('pushover-notifications')
 var sys = require('util')
-var express = require('express');
-var bodyParser = require('body-parser');
-var app = express();
+var express = require('express')
+var bodyParser = require('body-parser')
+var app = express()
 app.use(express.static('public'))
+app.use(bodyParser.json()) // support json encoded bodies
+app.use(bodyParser.urlencoded({ extended: true })) // support encoded bodies
 
-app.use(bodyParser.json()); // support json encoded bodies
-app.use(bodyParser.urlencoded({
-    extended: true
-})); // support encoded bodies
+var retries = 1
 
-var retries = 1;
-
+// Setup Handlebar Views
 app.engine('.hbs', exphbs({
     defaultLayout: 'main',
     extname: '.hbs',
@@ -24,83 +22,86 @@ app.engine('.hbs', exphbs({
 app.set('view engine', '.hbs')
 app.set('views', path.join(__dirname, 'views'))
 
-app.get('/', (request, response) => {
-    console.log("retries = ", retries);
-    if (retries == 1) {
-        response.render('login', {
-            title: "Enter Passphrase"
-        });
-    } else {
-        response.render('login', {
-            title: "Incorrect Passphrase, attempts: " + (retries - 1)
-        });
-    }
-})
-
-app.get('/success', (request, response) => {
-    var cmd = "sudo cryptsetup status securebackup ; echo ;  mount | grep securebackup ; echo ; fortune";
-    var exec = require('child_process').exec;
-    var output;
-    console.log("retries should be 0, but they're = ", retries);
-    if (retries == 0) {
-        exec(cmd, function callback(error, stdout, stderr) {
-            console.log("out:", stdout, "err:", stderr);
-            output = "Dry Run: " + mounted() + "\n\n" + stdout + stderr;
-            console.log("output is now: " + output);
-            retries += 1;
-            pushthis(output, "Offsite: Success Page");
-            response.render('success', {
-                output: output
-            });
-        });
-    } else {
-        response.redirect('/');
-    }
-})
 
 app.route({
     method: 'GET',
     path: '/',
     handler: function(request, reply) {
-        reply.view('login');
+        reply.view('login')
     }
-});
+})
+
+
+// Process main GET request
+app.get('/', (request, response) => {
+    console.log("retries = ", retries)
+    if (retries == 1) {
+        response.render('login', {
+            title: "Enter Passphrase"
+        })
+    } else {
+        response.render('login', {
+            title: "Incorrect Passphrase, attempts: " + (retries - 1)
+        })
+    }
+})
+
+app.get('/success', (request, response) => {
+    var cmd = "sudo cryptsetup status securebackup ; echo ;  mount | grep securebackup ; echo ; fortune"
+    var exec = require('child_process').exec
+    var output
+    console.log("retries should be 0, but they're = ", retries)
+    if (retries == 0) {
+        exec(cmd, function callback(error, stdout, stderr) {
+            console.log("out:", stdout, "err:", stderr)
+            output = "Dry Run: " + mounted() + "\n\n" + stdout + stderr
+            console.log("output is now: " + output)
+            retries += 1
+            pushthis(output, "Offsite: Success Page")
+            response.render('success', {
+                output: output
+            })
+        })
+    } else {
+        response.redirect('/')
+    }
+})
 
 
 app.post('/exec', function(req, res) {
     var p = req.body.passphrase
     var cmd = "echo -n \"" + p + "\" | sudo cryptsetup luksOpen /dev/sda1 securebackup --tries 1"
-    cmd2 = "sudo mount /dev/mapper/securebackup /secure";
+    cmd2 = "sudo mount /dev/mapper/securebackup /secure"
     if (mounted()) {
-        cmd += " --test-passphrase";
-        cmd2 = "sudo mount -f /dev/mapper/securebackup /secure";
+        cmd += " --test-passphrase"
+        cmd2 = "sudo mount -f /dev/mapper/securebackup /secure"
     }
 
-    console.log("before exec");
-    var exec = require('child_process').exec;
+    console.log("before exec")
+    var exec = require('child_process').exec
     exec(cmd, function callback(error, stdout, stderr) {
         if (!error) {
             exec(cmd2, function callback(error, stdout, stderr) {
-                console.log("executing: [" + cmd2 + "]");
-                retries = 0;
-                pushthis("Success!", "Offsite: Password Accepted");
-                console.log("redirecting to success page");
-                res.redirect("/success");
-            });
+                console.log("executing: [" + cmd2 + "]")
+                retries = 0
+                pushthis("Success!", "Offsite: Password Accepted")
+                console.log("redirecting to success page")
+                res.redirect("/success")
+            })
         } else {
-            retries += 1;
-            pushthis("Failure!", "Offsite: Incorrect Password");
-            res.redirect("/");
+            retries += 1
+            pushthis("Failure!", "Offsite: Incorrect Password")
+            res.redirect("/")
         }
 
-    });
+    })
 
-});
+})
 
 
-pushthis("Offsite: Starting");
+pushthis("Offsite: Starting")
 
-app.listen(8080, '127.0.0.1');
+app.listen(8080, '127.0.0.1')
 
 function pushthis(message, title) {
     var p = new push({
@@ -109,7 +110,7 @@ function pushthis(message, title) {
         // onerror: function(error) {},
         // update_sounds: true // update the list of sounds every day - will
         // prevent app from exiting.
-    });
+    })
 
     var msg = {
         // These values correspond to the parameters detailed on https://pushover.net/api
@@ -119,25 +120,25 @@ function pushthis(message, title) {
         sound: 'magic',
         device: 'Offsite',
         priority: 1
-    };
+    }
 
     p.send(msg, function(err, result) {
         if (err) {
-            throw err;
+            throw err
         }
 
-        console.log(result);
+        console.log(result)
 
-    });
+    })
 
 }
 
-var fs = require('fs');
+var fs = require('fs')
 
 function mounted() {
     try {
-        return fs.statSync('/secure/mounted').isFile();
+        return fs.statSync('/secure/mounted').isFile()
     } catch (err) {
-        return false;
+        return false
     }
 }
